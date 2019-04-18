d22b8fd4c16da725d0e7245ae37faab90dd88f62
Guangqi <363305719@qq.com>
2019-04-18 00:08:41 -0400

    finish css html check

--- lab9/hw9/Qing_Guangqi_CS546_B/routes/index.js
+++
@@ -1,9 +0,0 @@
-const constructorMethod = app=>{
-    app.get("/",(req, res) =>{
-        res.render("primeChecker/prime",{title:"Prime checker"});
-    });
-    app.use("*", (req, res) =>{
-        res.redirect("/");
-    });
-};
-module.exports = constructorMethod;
