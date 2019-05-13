d9156f9949218dd31edb2949da5edc55191326bb
Guangqi <363305719@qq.com>
2019-05-13 00:33:15 -0400

    rm final_backup

--- final/data/index.js
+++
@@ -1,9 +0,0 @@
-const usersData = require("./users");
-const userFlyersData = require("./userFlyers");
-const templateFlyersData = require("./templateFlyers");
-
-module.exports = {
-  users: usersData,
-  userFlyers: userFlyersData,
-  templateFlyers: templateFlyersData
-};
